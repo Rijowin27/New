da5e45b4f8de3e23c5f06cef6d6cc91aa83f78ba
Rijowin27 <727723euec157@skcet.ac.in>
2025-01-25 15:51:11 +0530

    Rock Paper Scissor

---
+++ random.js
@@ -0,0 +1,18 @@
+const user={
+    name:"Kendrick",
+    age:"35",
+    "is Singer":"true"
+}
+
+console.log(user.name);
+
+let a=25;
+let b=a;
+console.log(a);
+b=30;
+console.log(a);
+console.log(b);
+
+let user2=user.age;
+user2=32;
+console.log(user2);
